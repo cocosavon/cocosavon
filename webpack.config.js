aba6bce6fa1ec6334dd9aea4e9714d365717c9dd
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-07-18 22:00:51 +0800

    Make webpack command work

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,23 @@
 var path = require('path');
 
 module.exports = {
+    // It's better to set either development/production/none, otherwise you can get a warnining message
+    mode: 'development',
+
     devServer: {
+        // Server regards the contentBase as a root
+        contentBase: path.join(__dirname, 'public')  // when you access the root URL, the server looks for the file under contentBase folder
+    },
+
+    // Input file
+    entry: './src/index.js',
+
+    // Exported file
+    output: {
+        // Path to export.  This must be absolute path
+        path: path.join(__dirname, 'public/js'),
+
+        // Output file name.  All the files are merged into this file
+        filename: 'bundle.js',
     }
 };
