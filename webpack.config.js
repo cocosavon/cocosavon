b8cc9fc14081ccf4f50c4924511dafb1d8b89b2d
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-07-29 12:46:01 +0800

    Make docs folder document root so that the Github Pages can host index.html and bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,19 @@
 // Use this for the calculating the output path (absolute path)
 var path = require('path');
-const outputPath = path.resolve(__dirname, 'public');
-
+const outputPath = path.resolve(__dirname, 'docs');
 // vue-loader@15から必要
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
     // It's better to set either development/production/none, otherwise you can get a warnining message
     mode: 'development',
-
     devServer: {
         // webpack-dev-serverを立ち上げた時のドキュメントルートを設定
         // ここでは outputPath ディレクトリのindex.htmlにアクセスするよう設定してます
         contentBase: outputPath  // when you access the root URL, the server looks for the file under contentBase folder
     },
-
     // Input file
     entry: './src/main.js',
-
     // Exported file
     output: {
         // Path to export.  This must be absolute path
@@ -27,7 +23,6 @@ module.exports = {
         // Output file name.  All the files are merged into this file
         filename: 'bundle.js',
     },
-
     module:{
         rules: [
             {
@@ -43,9 +38,7 @@ module.exports = {
             }
         ]
     },
-
     plugins: [new VueLoaderPlugin()],
-
     resolve: {
         // import './foo.vue' の代わりに import './foo' と書けるようになる(拡張子省略)
         extensions: ['.js', '.vue'],
