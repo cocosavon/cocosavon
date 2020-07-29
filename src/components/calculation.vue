3a5d4e1ccff7faac4edd59ab107078160946413a
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-07-29 17:34:13 +0800

    Install Cloud-Firestore SDK

--- src/components/calculation.vue
+++ src/components/calculation.vue
@@ -5,11 +5,14 @@
 </template>
 
 <script>
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
 export default {
 
 }
 </script>
 
 <style lang="css">
-
 </style>
