f7f8666538bf4093e5e37961a5ac259d86473277
Katsufumi Shibata <katsufumi.shibata@dbca.wa.gov.au>
2020-11-16 14:29:22 +0800

    Remove transition

--- src/components/calculation.vue
+++ src/components/calculation.vue
@@ -22,7 +22,6 @@
                     <template v-else>
                         <template v-for="oil in oils_array">
                             <template v-if="oil.selected">
-                                <transition name="aho">
                                 <div class="container mb-2 rounded oil_wrapper" :class="get_being_input_class(oil)">
 <!--
                                     <div class="oil_close_icon" @click="oilCloseClicked($event, oil)">
@@ -80,7 +79,6 @@
                                         </div>
                                     </div>
                                 </div>
-                                </transition>
                             </template>
                             <template v-else>
                                 <div class="container mb-2">
